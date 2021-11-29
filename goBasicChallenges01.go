0c6da113bff8fc53a9b097ba4c95091428dcebca
E-Rock <E-Rock@users.noreply.github.com>
2021-11-29 18:57:40 +0000

    fixed fubar typing

---
+++ goBasicChallenges01.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main () {
+    name := "First and Last Name"
+    city := "City"
+    state := "State"
+
+    fmt.Printf("The name is: %s. \n", name)
+    fmt.Printf("The city is: %s. \n", city)
+    fmt.Printf("The state is: %s, \n", state)
+}
